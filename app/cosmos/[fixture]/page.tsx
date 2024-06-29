2349b64e2fb2203ecf3ccf05263eb51382cda989
Taehee Kim <rabolution@gmail.com>
2024-06-29 12:56:26 +0900

    react-cosmos storybook setting

---
+++ app/cosmos/[fixture]/page.tsx
@@ -0,0 +1,6 @@
+import { nextCosmosPage, nextCosmosStaticParams } from 'react-cosmos-next';
+import * as cosmosImports from '../../../cosmos.imports';
+ 
+export const generateStaticParams = nextCosmosStaticParams(cosmosImports);
+ 
+export default nextCosmosPage(cosmosImports);
