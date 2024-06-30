bb832adeb67a5dfc021261990c8abbfc91aace33
Taehee Kim <rabolution@gmail.com>
2024-06-30 13:34:49 +0900

    feat: 전체 필터

--- src/app/domains/saas/SaasList/SaasList.test.tsx
+++ src/app/domains/saas/SaasList/SaasList.test.tsx
@@ -5,18 +5,30 @@ import { expectTL } from "@/siheom/expectTL";
 import { queryTL } from "@/siheom/queryTL";
 
 describe("SaasList", () => {
-	test("최근 결제일 순으로 정렬된다", async () => {
+	test("결제 내역이 있는 SaaS만 필터할 수 있다", async () => {
 		render(SaasListStories["Saas가 여럿 있음"]);
 
+		// given 전체 SaaS 필터 체크됨
+		await expectTL(queryTL.radio("전체 5")).toBeChecked();
 		await expectTL(queryTL.list("SaaS 목록")).toBeVisible();
 
-		// given 전체 SaaS, 최근 결제일 순으로 정렬됨
+		// given 최근 결제일 순으로 정렬됨
 		await expectTL(queryTL.listitem("")).toHaveTextContents([
 			"Notion2024년 6월 27일 결제",
 			"Asana2024년 6월 20일 결제",
 			"GitHub2024년 6월 15일 결제",
 			"Slack",
 			"Zoom",
+		]);
+
+		// when 결제 내역 있는 SaaS 라디오를 클릭하면
+		await queryTL.radio("결제 내역 있는 SaaS 3").click();
+
+		// then 결제 내역이 있는 SaaS만 필터됨
+		await expectTL(queryTL.listitem("")).toHaveTextContents([
+			"Notion2024년 6월 27일 결제",
+			"Asana2024년 6월 20일 결제",
+			"GitHub2024년 6월 15일 결제",
 		]);
 	});
 });
--- src/app/domains/saas/SaasList/SaasList.tsx
+++ src/app/domains/saas/SaasList/SaasList.tsx
@@ -1,4 +1,7 @@
+"use client";
+import { VStack } from "@styled-system/jsx";
 import { SaasListItem } from "./SaasListItem";
+import { useState } from "react";
 
 export const SaasList = ({
 	saasList,
@@ -10,16 +13,35 @@ export const SaasList = ({
 		lastPaidAt: Date | null;
 	}[];
 }) => {
+	const [selected, setSelected] = useState("all");
+
 	return (
-		<ul aria-label="SaaS 목록">
-			{saasList
-				.sort(
-					(a, b) =>
-						(b.lastPaidAt?.valueOf() ?? 0) - (a.lastPaidAt?.valueOf() ?? 0),
-				)
-				.map((saas) => (
-					<SaasListItem key={saas.id} saas={saas} />
-				))}
-		</ul>
+		<VStack>
+			<fieldset>
+				<legend> 필터 </legend>
+				<label>
+					전체 {saasList.length}
+					<input
+						type="radio"
+						value="all"
+						checked={selected === "all"}
+						onChange={() => {
+							setSelected("all");
+						}}
+					/>
+				</label>
+			</fieldset>
+
+			<ul aria-label="SaaS 목록">
+				{saasList
+					.sort(
+						(a, b) =>
+							(b.lastPaidAt?.valueOf() ?? 0) - (a.lastPaidAt?.valueOf() ?? 0),
+					)
+					.map((saas) => (
+						<SaasListItem key={saas.id} saas={saas} />
+					))}
+			</ul>
+		</VStack>
 	);
 };
